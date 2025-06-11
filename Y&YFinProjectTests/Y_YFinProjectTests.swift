eb06c2fbc7db89988e4a52e5b2dc00a7d108989b
whyourelated <whyourelated@gmail.com>
2025-06-12 01:06:32 +0700

    Initial Commit

---
+++ Y&YFinProjectTests/Y_YFinProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Y_YFinProjectTests.swift
+//  Y&YFinProjectTests
+//
+//  Created by whyourelated on 12/06/25.
+//
+
+import Testing
+@testable import Y_YFinProject
+
+struct Y_YFinProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Y&YFinProjectUITests/Y_YFinProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Y_YFinProjectUITestsLaunchTests.swift
+//  Y&YFinProjectUITests
+//
+//  Created by whyourelated on 12/06/25.
+//
+
+import XCTest
+
+final class Y_YFinProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
